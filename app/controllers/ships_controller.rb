901acf8ebaa847c330f7ff9bf9a3b0c3a0b87d9a
safych <v7lad@ukr.net>
2021-12-17 23:21:11 +0200

    Create project and Controller

---
+++ app/controllers/ships_controller.rb
@@ -0,0 +1,25 @@
+class ShipsController < ApplicationController
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  def cargoe_params
+
+  end
+end
---
+++ app/helpers/cargoes_helper.rb
@@ -0,0 +1,2 @@
+module CargoesHelper
+end
